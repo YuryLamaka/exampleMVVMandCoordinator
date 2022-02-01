bc6a75d3477f161429616962a317222cd58f24dc
YuryLamaka <loko555@yahoo.com>
2022-02-01 23:03:22 +0300

    end work & bug fixes in AppDelegate

--- MVVMexampleSwift/AppDelegate.swift
+++ MVVMexampleSwift/AppDelegate.swift
@@ -10,15 +10,22 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+        var window: UIWindow?
+        var coordinator: Appcoordinator?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        
-        
-        return true
+            func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                
+                let navigationController = UINavigationController()
+                coordinator = Appcoordinator(navigationController: navigationController)
+                coordinator?.start()
+                window = UIWindow(frame: UIScreen.main.bounds)
+                window?.rootViewController = navigationController
+                window?.makeKeyAndVisible()
+                
+               return true
     }
-
+    
+    }
    
-}
+
 
